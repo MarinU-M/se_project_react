46a5a944b03020a906d9d3d8573e7c7feba36c7d
Marin M-U <marin.umegane.m@gmail.com>
2023-09-17 15:00:08 -0400

    feat: create file structure

---
+++ src/component/Main/Main.js
@@ -0,0 +1,2 @@
+//     The WeatherCard component, which shows the current temperature. Weather data is sent here, in addition to the Header, as props. Note that the weather data is not stored in Main.js, so you need to pass it down from the App component.
+// Clothing item cards, which are filtered based on the current weather. Wrap the ItemCard component into the unordered list and use the filter() and map() methods.
