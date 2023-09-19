75938ee1f227c1a0dc39628da043ae7e9833b940
Marin M-U <marin.umegane.m@gmail.com>
2023-09-19 12:04:51 -0400

    feat: create footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Header />
         <Main />
+        <Footer />
       </div>
     </>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,15 @@
 // A component that renders meta info about the app: the copyright and the production year. It consists of basic text.
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        <span>Developed by Marin Umegane-McGuinness</span>
+        <span>2023</span>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
