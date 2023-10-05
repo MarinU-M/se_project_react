e62f8c1a9c6dae138c5fd80bf9f733b1d4b43504
Marin M-U <marin.umegane.m@gmail.com>
2023-10-05 12:16:42 -0400

    fix: fix deployment

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,4 @@
-//     The WeatherCard component, which shows the current temperature. Weather data is sent here, in addition to the Header, as props.
-// Note that the weather data is not stored in Main.js, so you need to pass it down from the App component.
-
-// Wrap the ItemCard component into the unordered list and use the filter() and map() methods.
 import { useMemo, useContext } from "react";
-// import { defaultClothingItems } from "../../utils/constants";
 import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
